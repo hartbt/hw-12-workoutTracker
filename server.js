55a72987e4e0a7d8ff03bb2cbd26c2d7dc1a0e10
Brendon Hart <brendonhartbusiness@gmail.com>
2020-11-16 15:16:35 -0700

    Started working on server.js file

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 var express = require("express");
 var app = express();
 var logger = require("morgan")
@@ -6,12 +5,26 @@ var mongoose = require("mongoose")
 
 const PORT = process.env.PORT || 3000
 
+app.use(logger("dev"))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(require("./routes/html-routes"))
+app.use(require("./routes/api-routes"))
+
+
 
 app.get("/", function(req, res) {
   res.send("");
+})
 
 // Listener
 // ===========================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
-}):
+})
