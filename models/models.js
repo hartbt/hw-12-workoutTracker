f5cb64241303a656009d34657662661a537160d8
Brendon Hart <brendonhartbusiness@gmail.com>
2020-11-16 15:48:16 -0700

    Added workoutSchema things

--- models/models.js
+++ models/models.js
@@ -43,6 +43,18 @@ const workoutSchema = new Schema(
             }
         ]
 
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }
     }
 )
+
+workoutSchema.virtual("totalDuration").get(function(){
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration
+    }, 0)
+})
+
 
